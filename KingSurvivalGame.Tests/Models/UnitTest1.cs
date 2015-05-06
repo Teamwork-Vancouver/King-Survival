c119dfb343d1f135b242573777520c072eeacb76
Pe6o601 <petermarinov93@abv.bg>
2015-05-06 21:11:31 +0300

    board tested and changed IsPositionInBounds
    board tested and changed IsPositionInBounds

--- KingSurvivalGame.Tests/Models/UnitTest1.cs
+++ KingSurvivalGame.Tests/Models/UnitTest1.cs
@@ -4,17 +4,20 @@
     using KingSurvival.Contracts;
     using KingSurvival.Enumerations;
     using KingSurvival.Models;
+    using System.Linq;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using KingSurvival;
 
     [TestClass]
     public class BoardTests
     {
-        public Dictionary<Position, IFigure> Figures;
+        public Board board;
+        public Dictionary<Position, IFigure> figures;
 
         [TestInitialize]
         public void InitFigures()
         {
-            this.Figures = new Dictionary<Position, IFigure>();
+            this.board = new Board(GameConstants.BoardWidth, GameConstants.BoardHeight);
         }
 
         [TestMethod]
@@ -22,9 +25,51 @@
         {
             Position position = new Position(1, 1, 7, 7);
             Figure figure = new King((char)FigureSymbols.King);
-            this.Figures.Add(position, figure);
+            this.board.Figures.Add(position, figure);
 
-            Assert.AreEqual(1, this.Figures.Count, "The count shoud be 1 but it is not!");
+            Assert.AreEqual(6, this.board.Figures.Count, "The count shoud be 1 but it is not!");
+        }
+
+        [TestMethod]
+        public void CheckIfIsPositionInBoundReturnsTheRightResult()
+        {
+            int maxBoundsX = 7;
+            int maxBoundsY = 7;
+            //var keys = this.board.Figures.Keys;
+            for (int i = 0; i <= maxBoundsX; i++)
+            {
+                for (int j = 0; j <= maxBoundsY; j++)
+                {
+                    Assert.AreEqual(true, this.board.IsPositionInBounds(i, j), "The method shoud return true");
+                }
+            }
+        }
+
+        [TestMethod]
+        public void CheckIfPositionIsAvailableReturnsTheRightResults()
+        {
+            int maxBoundsX = 7;
+            int maxBoundsY = 7;
+            var keys = this.board.Figures.Keys;
+            for (int i = 0; i <= maxBoundsX; i++)
+            {
+                for (int j = 0; j <= maxBoundsY; j++)
+                {
+
+
+                    if (keys.ToArray().FirstOrDefault(obj => obj.X == i && obj.Y == j) != null)
+                    {
+                        Assert.AreEqual(false, this.board.IsPositionAvailable(i, j), "this method shoud have returned false");
+                    }
+                    else
+                    {
+                        Assert.AreEqual(true, this.board.IsPositionAvailable(i, j), "This method shoud have return true");
+                    }
+                    //Assert.AreEqual(true, this.board.IsPositionInBounds(i, j), "The method shoud return true");
+                }
+            }
         }
     }
 }
+
+
